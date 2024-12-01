564127282122805b68ae86b69f6077582ba1c42b
DLOADIN <m.david@alustudent.com>
2024-12-01 21:41:31 +0200

    new commit

---
+++ JAVASCRIPT/14.Sleep.js
@@ -0,0 +1,8 @@
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+class Solution{
+  async sleep(millis, value) {
+    return new Promise(resolve => setTimeout(() => resolve(value), millis));
+  }
+    
+}
